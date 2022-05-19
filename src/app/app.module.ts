e5ff47bef3d709f60fa0c3add75cb353a51f1cd4
valentinesamuel <valentinesamuel2580@gmail.com>
2022-05-19 15:38:18 +0100

    FEAT: Created all necessaru folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { PatientListComponent } from './patient/patient-list/patient-list.component';
+import { RegisterPatientComponent } from './patient/register-patient/register-patient.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PatientListComponent,
+    RegisterPatientComponent,
+    HeaderComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
