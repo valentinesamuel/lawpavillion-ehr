4d37bb229111d31acd6201ec24cb8e11179bc9ce
valentinesamuel <valentinesamuel2580@gmail.com>
2022-05-20 14:13:58 +0100

    ADDED; Finished stepper and its forms

--- src/app/patient/register-patient/register-patient.component.ts
+++ src/app/patient/register-patient/register-patient.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register-patient',
@@ -7,12 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPatientComponent implements OnInit {
   step = 1;
-  selected:any
+  selected: any;
+  titles = ['Mr', 'Mrs', 'Master', 'Miss'];
+  genders = ['Gender', 'Male', 'Female', 'Other'];
+  ethnicities = ['igbo', 'yoruba', 'hausa', 'german', 'british', 'american'];
+  religion = ['christainity', 'muslim', 'hinduism', ' stoicism', 'budduhism'];
+  maritalStatus = ['single', 'married', 'engaged'];
+  nationality = ['nigeia', 'america', 'united kingdom', 'israel', 'canada'];
+  stateOfOrigin = ['alabama', 'lagos', 'abuja', 'manchester', 'rivers'];
+  localGovernment = [
+    'ijebu-ode',
+    'ikoba-okha',
+    'oredo',
+    'uvwie',
+    'obio-akpor',
+    'khana',
+    'okpe',
+  ];
+  stateofResidence = ['alabama', 'lagos', 'abuja', 'manchester', 'rivers'];
+  relationship = ['mother', 'father', 'brother', 'sister'];
+  sponsorBillingPolicy = ['tied', 'owed', 'omo', 'e choke'];
+
+  addPatientForm:FormGroup
+
   constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+
+
+  }
   selectStep(step: any) {
-    this.step = step
-    this.selected = !this.selected
+    this.step = step;
+    this.selected = !this.selected;
   }
 }
